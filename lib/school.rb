aa4936290ee111a56018d095b34eded6641b1a48
Melissa Faulner <melissa.faulner@gmail.com>
2020-12-13 13:40:16 -0500

    commit tree 614d7fc551a2a596f6bb522ac07efa12cf8a0879

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, student_grade)
+        roster[student_grade] ||= []
+        roster[student_grade] << student_name
+    end
+
+    def grade(grade_param)
+        roster[grade_param]      
+    end
+
+    def sort
+        sorted_roster = {}
+        roster.each do |grade, students_in_grade|
+            sorted_roster[grade] = students_in_grade.sort
+        end
+        sorted_roster
+    end
+
+end
+
+
+# binding.pry
+# 0
